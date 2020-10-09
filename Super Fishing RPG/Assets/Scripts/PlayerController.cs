e5db3edd16ab4eb66af1cb7f09c185ab627e3ad3
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-10-09 02:22:30 -0600

    Started implementing player movement and tested the collision with the player.

---
+++ Super Fishing RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    Rigidbody2D myRigidBody;
+
+    [SerializeField] float runSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        myRigidBody.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Player is on: " + collision.gameObject.name);
+    }
+}
