6881474e9cd2d2e377a3fcf896eba56ccf3cce77
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-10-10 06:45:54 -0600

    ++Figured out how to add C# events with the input system. ++Made a dynamic shader for showing which interactable the player can use. ++Made the code that changes which one is that interactable. ++Started outlining code for

---
+++ Super Fishing RPG/Assets/Scripts/PlayerBoat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoat : MonoBehaviour, IInteractable
+{
+    [SerializeField] Material selectedMaterial;
+    public PlayerController interactingPlayer;
+    Material defaultMaterial;
+    SpriteRenderer spriteRenderer;
+    bool isDocked = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultMaterial = spriteRenderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Interact()
+    {
+        Debug.Log("Activate boat");
+    }
+
+    public void Highlight(bool isSelected)
+    {
+        if (isSelected)
+        {
+            spriteRenderer.material = selectedMaterial; 
+        }
+        else
+        {
+            spriteRenderer.material = defaultMaterial;
+        }
+    }
+}
--- Super Fishing RPG/Assets/Scripts/PlayerController.cs
+++ Super Fishing RPG/Assets/Scripts/PlayerController.cs
@@ -2,40 +2,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    PlayerInput playerInput;
+    PlayerControls controls;
+    Animator myAnimator;
+    Rigidbody2D myRigidBody;
+    LayerMask interactableLayerMask;
+    IInteractable nearbyInteractable;
+
     float horizontal;
     float vertical;
-    Rigidbody2D myRigidBody;
 
-    [SerializeField] float runSpeed = 20.0f;
+    [SerializeField] float moveSpeed = 20.0f;
+
+    public IInteractable NearbyInteractable { get => nearbyInteractable; set => nearbyInteractable = value; }
+
+    private void OnEnable()
+    {
+        controls = new PlayerControls();
+        controls.Player.Enable();
+        controls.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        interactableLayerMask = LayerMask.GetMask("Interactable");
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        Move();
+        var moveVector = controls.Player.Move.ReadValue<Vector2>() * (moveSpeed * Time.deltaTime);
+        myRigidBody.velocity = moveVector;
+
+        myAnimator.SetFloat("XDir", moveVector.x);
+        myAnimator.SetFloat("YDir", moveVector.y);
     }
 
-    private void Move()
+    private void Interact_performed(InputAction.CallbackContext ctx)
     {
-        myRigidBody.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (nearbyInteractable != null)
+        {
+            nearbyInteractable.Interact();
+        }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("Player is on: " + collision.gameObject.name);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            if (nearbyInteractable == null)
+            {
+                nearbyInteractable = collision.GetComponent<IInteractable>();
+                nearbyInteractable.Highlight(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        {
+            nearbyInteractable.Highlight(false);
+            nearbyInteractable = null;
+        }
     }
 }
