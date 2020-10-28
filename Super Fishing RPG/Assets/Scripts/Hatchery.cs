c240ed3fc01776df10b18b47c07a47479a23e908
Hayden Matthews <haydenamatthews@yahoo.com>
2020-10-27 20:02:09 -0600

    Started working on spawning locations for fish.
    Started by adding a bounding box for the 'hatchery'.

---
+++ Super Fishing RPG/Assets/Scripts/Hatchery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hatchery : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 0));
+    } 
+#endif
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
