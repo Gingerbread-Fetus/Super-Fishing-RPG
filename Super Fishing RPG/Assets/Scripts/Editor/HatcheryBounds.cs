241f7c8e820fc398930cc5954eea2be6c1ce0ecb
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-10-29 16:04:24 -0600

    Created a hatchery object and made bounding box for the it
    What still needs to be done is making fish stay inside of the bounding box.

---
+++ Super Fishing RPG/Assets/Scripts/Editor/HatcheryBounds.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+[CustomEditor(typeof(Hatchery)), CanEditMultipleObjects]
+public class HatcheryBounds : Editor
+{
+    private BoxBoundsHandle m_BoundsHandle = new BoxBoundsHandle();
+
+    protected virtual void OnSceneGUI()
+    {
+        Hatchery hatchery = (Hatchery)target;
+
+        // copy the target object's data to the handle
+        m_BoundsHandle.center = hatchery.transform.position;
+        m_BoundsHandle.size = hatchery.Extents;
+        // draw the handle
+        EditorGUI.BeginChangeCheck();
+        m_BoundsHandle.DrawHandle();
+        if (EditorGUI.EndChangeCheck())
+        {
+            // record the target object before setting new values so changes can be undone/redone
+            Undo.RecordObject(hatchery, "Change Bounds");
+
+            // copy the handle's updated data back to the target object
+            Bounds newBounds = new Bounds();
+            newBounds.center = m_BoundsHandle.center;
+            newBounds.size = m_BoundsHandle.size;
+            hatchery.Extents = newBounds.size;
+            hatchery.transform.position = newBounds.center;
+        }
+    }
+}
--- Super Fishing RPG/Assets/Scripts/Hatchery.cs
+++ Super Fishing RPG/Assets/Scripts/Hatchery.cs
@@ -1,26 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Hatchery : MonoBehaviour
 {
-#if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 0));
-    } 
-#endif
+    public Bounds bounds {  get { return m_Bounds; } set { m_Bounds = value; } }
+    public Vector2 Extents { get => extents; set => extents = value; }
+
+    [SerializeField]
+    Vector2 extents = Vector2.one;
+    Bounds m_Bounds = new Bounds(Vector3.zero, Vector3.one);
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {    
     }
 }
